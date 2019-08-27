299d682680cd63cbeda4ab756006402854913e1a
luanaimer <luanaimer@gmail.com>
2019-08-26 22:00:04 -0300

    exercício IMC

---
+++ app/src/main/java/br/edu/ifsc/relativelayout_luanaoc/creditosActivity.java
@@ -0,0 +1,14 @@
+package br.edu.ifsc.relativelayout_luanaoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class creditosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creditos);
+    }
+}
